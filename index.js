926603074a1626da24b4dc9e94fede2357f37301
Martin Kolárik <martin@kolarik.sk>
2021-10-28 15:57:29 +0200

    Preserve boolean and number types

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const _ = require('lodash');
 
-module.exports = (function map (object, prefix = '') {
+module.exports = function map (object, types = true, prefix = '') {
 	return _.mapValues(object, (value, key) => {
+		key = (prefix ? `${prefix}_` : '') + _.snakeCase(key).toUpperCase();
+
+		// nested object
 		if (_.isObject(value)) {
-			return map(value, (prefix ? `${prefix}_` : '') + _.snakeCase(key).toUpperCase());
+			return map(value, types, key);
 		}
 
-		return (prefix ? `${prefix}_` : '') + _.snakeCase(key).toUpperCase();
+		// typed primitive
+		if (types && [ 'boolean', 'number' ].includes(typeof value)) {
+			return {
+				__name: key,
+				__format: typeof value,
+			};
+		}
+
+		// simple string
+		return key;
 	});
-});
+};
