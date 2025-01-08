f9f19c5af94dd449b80c886c282eaaf6f26240ba
Martin Kolárik <martin@kolarik.sk>
2025-01-08 14:55:41 +0100

    Warn when attempting to map arrays

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ module.exports = function map (object, types = true, prefix = '') {
 	return _.mapValues(object, (value, key) => {
 		key = (prefix ? `${prefix}_` : '') + _.snakeCase(key).toUpperCase();
 
+		// array get converted to objects when mapped, which may lead to hard-to-discover bugs
+		if (Array.isArray(value)) {
+			if (value.some(item => _.isObject(item))) {
+				console.warn(`Warning: Mapping individual array items to environment variables is not supported. You may override the whole array via ${key} or use an object with numeric keys instead.`);
+			}
+
+			return key;
+		}
+
 		// nested object
 		if (_.isObject(value)) {
 			return map(value, types, key);
